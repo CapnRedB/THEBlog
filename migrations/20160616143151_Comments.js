c9b546af0decfc72a6d8429ef6a9be43d3c566c5
Aaron Levinson <aaron.levinson1000@gmail.com>
2016-06-17 15:16:23 -0500

    Fixed seed issue

--- migrations/20160616143151_Comments.js
+++ migrations/20160616143151_Comments.js
@@ -2,7 +2,8 @@ exports.up = function(knex, Promise) {
 	return knex.schema.createTable("comments", function(table) {
 		table.increments().primary(),
 			table.string("body"),
-			table.integer("user_id").unsigned().index().references('users.id'),
+			table.integer("user_id").unsigned().index().references('users.id').onDelete(
+				'cascade'),
 			table.timestamp('created_at').defaultTo(knex.fn.now()),
 			table.integer("posts_id").unsigned().index().references('posts.id').onDelete(
 				'cascade')
--- seeds/dummy.js
+++ seeds/dummy.js
@@ -2,6 +2,8 @@ exports.seed = function(knex, Promise) {
 	return Promise.join(
 		// Deletes ALL existing entries
 		knex('users').del(),
+		knex('posts').del(),
+		knex('comments').del(),
 
 		// Inserts seed entries
 		knex('users').insert({
@@ -21,6 +23,50 @@ exports.seed = function(knex, Promise) {
 			full_name: 'rowValue3',
 			username: 'Bobby'
 
+		}),
+
+
+		// Inserts seed entries
+		knex('posts').insert({
+			id: 1,
+			title: 'rowValue',
+			content: "blah blah blah blah",
+			user_id: 1
+		}),
+		knex('posts').insert({
+			id: 2,
+			title: 'rowValue2',
+			content: "blah  we  blah blah blah",
+			user_id: 1
+
+		}),
+		knex('posts').insert({
+			id: 3,
+			title: 'rowValue3',
+			content: "blah blah weee blah blah",
+			user_id: 2
+
+		}),
+
+
+		// Inserts seed entries
+		knex('comments').insert({
+			id: 1,
+			body: 'blah blah trash post blah',
+			user_id: 1,
+			posts_id: 1
+		}),
+		knex('comments').insert({
+			id: 2,
+			body: 'blah blah good post blah',
+			user_id: 1,
+			posts_id: 2
+		}),
+		knex('comments').insert({
+			id: 3,
+			body: 'blah blah more trash post blah',
+			user_id: 2,
+			posts_id: 1
 		})
 	);
 };
