8d46cebb4c8c5104fd9776f48a4b6896396aa5f6
Aaron Levinson <aaron.levinson1000@gmail.com>
2016-06-17 10:39:29 -0500

    Heroku pls

--- app.js
+++ app.js
@@ -58,8 +58,11 @@ app.use(function(err, req, res, next) {
 		error: {}
 	});
 });
-app.listen(3000, function() {
-	console.log('Listening on port 3000');
+
+app.set('port', (process.env.PORT || 3000))
+
+app.listen(app.get('port'), function() {
+	console.log('Listening on port' + app.get('port'));
 })
 
 module.exports = app;
